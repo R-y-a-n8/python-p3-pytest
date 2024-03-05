9bdcdd2cd16dd22e2c14830d772931a75274b7d3
devRyan <ryan.kinuthia@student.moringaschool.com>
2024-03-06 02:34:38 +0300

    pytest

--- lib/testing/subdirectory/bool_test.py
+++ lib/testing/subdirectory/bool_test.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
-from bool_functions import return_true
+import unittest
+from bool_functions import return_true  # Import the function you want to test
 
-def test_return_true():
-    '''in bool_functions, function "return_true" returns True.'''
-    assert return_true() == True
+class TestBoolFunctions(unittest.TestCase):
+    def test_return_true(self):
+        '''in bool_functions, function "return_true" returns True.'''
+        self.assertTrue(return_true())
+
+if __name__ == '__main__':
+    unittest.main()
+
--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+import unittest
 from not_none_functions import return_not_none
 
-# def test_return_not_none():
-#     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
-#     assert False
+class TestNotNoneFunctions(unittest.TestCase):
+    def test_return_not_none(self):
+        '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
+        result = return_not_none()
+        self.assertIsNotNone(result)
+
+if __name__ == '__main__':
+    unittest.main()
+
